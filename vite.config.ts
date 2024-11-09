4544921f7b853f5ab115f41a91288ecb42188bd1
rizkyharahap <rizkyhh1999@gmail.com>
2024-11-09 17:46:51 +0700

    fix: add hash router for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import svgLoader from "vite-svg-loader";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/digital-invitation-gian/",
+  base: process.env.NODE_ENV === "production" ? "/digital-invitation-gian/" : "",
   plugins: [vue(), svgLoader(), vueDevTools()],
   resolve: {
     alias: {
