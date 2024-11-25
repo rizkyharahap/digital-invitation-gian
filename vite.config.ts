93b1ff02959f126bdfc34397b8281cf423b1bcdc
rizkyharahap <rizkyhh1999@gmail.com>
2024-11-25 22:12:46 +0700

    fix: change base path

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import svgLoader from "vite-svg-loader";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "",
+  base: "/",
   plugins: [vue(), svgLoader(), vueDevTools()],
   resolve: {
     alias: {
