2dea80e289e94967106d4f1d16f564ee3cdc2867
rizkyharahap <rizkyhh1999@gmail.com>
2024-11-25 20:12:31 +0700

    fix: change base path and createWebHasHistory to createWebHistory

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import svgLoader from "vite-svg-loader";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/digital-invitation-gian/" : "",
+  base: "",
   plugins: [vue(), svgLoader(), vueDevTools()],
   resolve: {
     alias: {
